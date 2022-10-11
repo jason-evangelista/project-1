46d080f8136232f3ce3ec644000cc72445b46faf
jason-evangelista <evangelistajasonglenn@gmail.com>
2022-10-11 18:39:45 +0800

    ⚡️ Implement filter search functionality

--- components/page-component/food/Food.tsx
+++ components/page-component/food/Food.tsx
@@ -1,15 +1,36 @@
-import { FC } from "react";
+import { FC, useMemo, useState } from "react";
 import Layout from "../../Layout";
-import foodList from "../../../api/food";
+import foodList, { FoodInfo } from "../../../api/food";
 import FoodCard from "./component/FoodCard";
+import FoodSearchInput from "./component/FoodSearchInput";
+import utilStyle from "../../../styles/utils.module.css";
 
 const Food: FC = () => {
+  const [food] = useState<FoodInfo[]>(foodList);
+  const [query, setSearchQuery] = useState("");
+
+  const memoizedFoodList = useMemo(() => {
+    const result = food.filter((item) =>
+      item.title.match(new RegExp(query, "i"))
+    );
+    return result;
+  }, [food, query]);
+
+  const isEmptyResult = !memoizedFoodList.length;
+
   return (
     <Layout home={false}>
       <div>
-        {foodList.map((item) => (
-          <FoodCard {...item} key={item.title} />
-        ))}
+        <FoodSearchInput handleSearchField={setSearchQuery} />
+        {isEmptyResult ? (
+          <h1 className={utilStyle.textCenter}>Empty Result</h1>
+        ) : (
+          <>
+            {memoizedFoodList.map((item) => (
+              <FoodCard {...item} key={item.title} />
+            ))}
+          </>
+        )}
       </div>
     </Layout>
   );
